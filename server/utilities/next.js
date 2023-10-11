584230bb8010a469e6d85465625d6511d06da619
Lucas <l.vanegeraat@gmail.com>
2023-10-11 08:34:03 +0400

    Logging

--- server/utilities/next.js
+++ server/utilities/next.js
@@ -1,22 +1,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getPageModule = exports.resolvePathname = exports.getHttpServer = void 0;
+exports.getPageModule =
+  exports.resolvePathname =
+  exports.getHttpServer =
+    void 0;
 const tslib_1 = require("tslib");
 const node_http_1 = require("node:http");
-const next_server_1 = tslib_1.__importDefault(require("next/dist/server/next-server"));
+const next_server_1 = tslib_1.__importDefault(
+  require("next/dist/server/next-server")
+);
 /**
  * Get the http.Server instance from the NextNodeServer.
  * @param nextServer The NextNodeServer instance.
  * @returns The http.Server instance.
  */
 function getHttpServer(nextServer) {
-    if (!nextServer || !(nextServer instanceof next_server_1.default))
-        throw new Error('Next WS is missing access to the NextNodeServer');
-    // @ts-expect-error - serverOptions is protected
-    const httpServer = nextServer.serverOptions?.httpServer;
-    if (!httpServer || !(httpServer instanceof node_http_1.Server))
-        throw new Error('Next WS is missing access to the http.Server');
-    return httpServer;
+  if (!nextServer || !(nextServer instanceof next_server_1.default))
+    throw new Error("Next WS is missing access to the NextNodeServer");
+  // @ts-expect-error - serverOptions is protected
+  const httpServer = nextServer.serverOptions?.httpServer;
+
+  console.log(33, nextServer.serverOptions);
+  
+  if (!httpServer || !(httpServer instanceof node_http_1.Server))
+    throw new Error("Next WS is missing access to the http.Server");
+  return httpServer;
 }
 exports.getHttpServer = getHttpServer;
 /**
@@ -26,45 +34,37 @@ exports.getHttpServer = getHttpServer;
  * @returns The resolved page, or null if the page could not be resolved.
  */
 function resolvePathname(nextServer, pathname) {
-    if (pathname.startsWith('/_next'))
-        return null;
-    const pathParts = pathname.split('/');
-    const appRoutes = {
-        // @ts-expect-error - appPathRoutes is protected
-        ...nextServer.appPathRoutes,
-        // @ts-expect-error - getAppPathRoutes is protected
-        ...nextServer.getAppPathRoutes(),
-    };
-    for (const [key, [path]] of Object.entries(appRoutes)) {
-        const hasDynamic = key.includes('[') && key.includes(']');
-        if (hasDynamic) {
-            const keyParts = key.split('/');
-            if (keyParts.length !== pathParts.length)
-                continue;
-            for (let i = 0; i < keyParts.length; i++) {
-                const keyPart = keyParts[i];
-                const pathPart = pathParts[i];
-                const isDynamic = keyPart.includes('[') && keyPart.includes(']');
-                if (isDynamic)
-                    keyParts[i] = pathPart;
-                if (keyParts[i] !== pathParts[i])
-                    break;
-                if (i === keyParts.length - 1) {
-                    if (!path?.endsWith('/route'))
-                        return null;
-                    return path;
-                }
-            }
+  if (pathname.startsWith("/_next")) return null;
+  const pathParts = pathname.split("/");
+  const appRoutes = {
+    // @ts-expect-error - appPathRoutes is protected
+    ...nextServer.appPathRoutes,
+    // @ts-expect-error - getAppPathRoutes is protected
+    ...nextServer.getAppPathRoutes(),
+  };
+  for (const [key, [path]] of Object.entries(appRoutes)) {
+    const hasDynamic = key.includes("[") && key.includes("]");
+    if (hasDynamic) {
+      const keyParts = key.split("/");
+      if (keyParts.length !== pathParts.length) continue;
+      for (let i = 0; i < keyParts.length; i++) {
+        const keyPart = keyParts[i];
+        const pathPart = pathParts[i];
+        const isDynamic = keyPart.includes("[") && keyPart.includes("]");
+        if (isDynamic) keyParts[i] = pathPart;
+        if (keyParts[i] !== pathParts[i]) break;
+        if (i === keyParts.length - 1) {
+          if (!path?.endsWith("/route")) return null;
+          return path;
         }
-        else {
-            if (key !== pathname)
-                continue;
-            if (!path?.endsWith('/route'))
-                return null;
-            return path;
-        }
+      }
+    } else {
+      if (key !== pathname) continue;
+      if (!path?.endsWith("/route")) return null;
+      return path;
     }
-    return null;
+  }
+  return null;
 }
 exports.resolvePathname = resolvePathname;
 /**
@@ -74,15 +74,15 @@ exports.resolvePathname = resolvePathname;
  * @returns The page module.
  */
 async function getPageModule(nextServer, filename) {
-    // @ts-expect-error - hotReloader is private
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-    await nextServer.hotReloader?.ensurePage({
-        page: filename,
-        clientOnly: false,
-    });
-    // @ts-expect-error - getPagePath is protected
-    const builtPagePath = nextServer.getPagePath(filename);
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    return require(builtPagePath);
+  // @ts-expect-error - hotReloader is private
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  await nextServer.hotReloader?.ensurePage({
+    page: filename,
+    clientOnly: false,
+  });
+  // @ts-expect-error - getPagePath is protected
+  const builtPagePath = nextServer.getPagePath(filename);
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  return require(builtPagePath);
 }
 exports.getPageModule = getPageModule;
--- src/server/utilities/next.ts
+++ src/server/utilities/next.ts
@@ -1,6 +1,6 @@
-import { Server } from 'node:http';
-import NextNodeServer from 'next/dist/server/next-server';
-import type { SocketHandler } from './ws';
+import { Server } from "node:http";
+import NextNodeServer from "next/dist/server/next-server";
+import type { SocketHandler } from "./ws";
 
 /**
  * Get the http.Server instance from the NextNodeServer.
@@ -9,12 +9,12 @@ import type { SocketHandler } from './ws';
  */
 export function getHttpServer(nextServer: NextNodeServer) {
   if (!nextServer || !(nextServer instanceof NextNodeServer))
-    throw new Error('Next WS is missing access to the NextNodeServer');
+    throw new Error("Next WS is missing access to the NextNodeServer");
 
   // @ts-expect-error - serverOptions is protected
   const httpServer = nextServer.serverOptions?.httpServer;
   if (!httpServer || !(httpServer instanceof Server))
-    throw new Error('Next WS is missing access to the http.Server');
+    throw new Error("Next WS is missing access to the http.Server");
 
   return httpServer;
 }
@@ -26,9 +26,9 @@ export function getHttpServer(nextServer: NextNodeServer) {
  * @returns The resolved page, or null if the page could not be resolved.
  */
 export function resolvePathname(nextServer: NextNodeServer, pathname: string) {
-  if (pathname.startsWith('/_next')) return null;
+  if (pathname.startsWith("/_next")) return null;
 
-  const pathParts = pathname.split('/');
+  const pathParts = pathname.split("/");
   const appRoutes = {
     // @ts-expect-error - appPathRoutes is protected
     ...nextServer.appPathRoutes,
@@ -37,28 +37,28 @@ export function resolvePathname(nextServer: NextNodeServer, pathname: string) {
   };
 
   for (const [key, [path]] of Object.entries(appRoutes)) {
-    const hasDynamic = key.includes('[') && key.includes(']');
+    const hasDynamic = key.includes("[") && key.includes("]");
 
     if (hasDynamic) {
-      const keyParts = key.split('/');
+      const keyParts = key.split("/");
       if (keyParts.length !== pathParts.length) continue;
 
       for (let i = 0; i < keyParts.length; i++) {
         const keyPart = keyParts[i];
         const pathPart = pathParts[i];
 
-        const isDynamic = keyPart.includes('[') && keyPart.includes(']');
+        const isDynamic = keyPart.includes("[") && keyPart.includes("]");
         if (isDynamic) keyParts[i] = pathPart;
         if (keyParts[i] !== pathParts[i]) break;
 
         if (i === keyParts.length - 1) {
-          if (!path?.endsWith('/route')) return null;
+          if (!path?.endsWith("/route")) return null;
           return path;
         }
       }
     } else {
       if (key !== pathname) continue;
-      if (!path?.endsWith('/route')) return null;
+      if (!path?.endsWith("/route")) return null;
       return path;
     }
   }
